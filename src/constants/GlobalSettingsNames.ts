102207576d11585163e626a370ba59d5df5fc6e2
Aidam1 <adam.znamenacek@gmail.com>
2020-10-12 12:09:03 +0200

    Added GlobalSettingsNames constants

---
+++ src/constants/GlobalSettingsNames.ts
@@ -0,0 +1,28 @@
+class GlobalSettingsNames {
+    static adminAppInactiveLogoutTime = 'AdminAppInactiveLogoutTime';
+    static exchangeRatesAdminGroupName = 'ExchangeRatesAdminGroupName';
+    static forcedEmailTrackingGroups = 'ForcedEmailTrackingGroups';
+    static groupsForAllUnpaidInvoicesNotification = 'GroupsForAllUnpaidInvoicesNotification';
+    static leadDeadlineAlertGroups = 'LeadDeadlineAlertGroups';
+    static minimumPasswordLength = 'MinimumPasswordLength';
+    static notifyAboutInvoicedInvoiceInPohodaGroup = 'NotifyAboutInvoicedInvoiceInPohodaGroup';
+    static notifyAboutPaidInvoiceInPohodaGroup = 'NotifyAboutPaidInvoiceInPohodaGroup';
+    static phoneListTaskSolverGroup = 'PhoneListTaskSolverGroup';
+    static projectDeadlineAlert = 'ProjectDeadlineAlert';
+    static serverUpdateProgressNotificationGroup = 'ServerUpdateProgressNotificationGroup';
+    static systemHealthNotificationGroup = 'SystemHealthNotificationGroup';
+    static defaultLanguage = 'DefaultLanguage';
+    static defaultCurrency = 'DefaultCurrency';
+    static myCompanyCountry = 'MyCompanyCountry';
+    static myCompanyName = 'MyCompanyName';
+    static myCompanyStreet = 'MyCompanyStreet';
+    static myCompanyCity = 'MyCompanyCity';
+    static myCompanyState = 'MyCompanyState';
+    static myCompanyZip = 'MyCompanyZIP';
+    static myCompanyId = 'MyCompanyID';
+    static myCompanyVat = 'MyCompanyVAT';
+    static createCompanyWhileImportingContactFromOutlook = 'CreateCompanyWhileImportingContactFromOutlook';
+    static applyGeneralDataProtectionRules = 'ApplyGeneralDataProtectionRules';
+}
+
+export default GlobalSettingsNames
