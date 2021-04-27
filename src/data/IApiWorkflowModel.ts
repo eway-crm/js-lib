c9f37aa5ff9dfa601841d984dad81071b4dfcf40
Aidam1 <adam.znamenacek@gmail.com>
2021-04-27 10:39:37 +0200

    refs #39889: Remove IsSystem which is no longer used

--- src/data/IApiWorkflowModel.ts
+++ src/data/IApiWorkflowModel.ts
@@ -2,7 +2,6 @@ import { IApiItemBase } from "./IApiItemBase";
 
 export interface IApiWorkflowModel extends IApiItemBase {
     EnumTypeGuid: string;
-    IsSystem: boolean;
     IsVisible: boolean;
     ParentEn: string;
 }
