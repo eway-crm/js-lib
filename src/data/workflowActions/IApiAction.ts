d3327e4bdd6aaa0727ce59d7354c7c33411a6292
Štěpán Havránek <stepan.havranek@eway-crm.com>
2021-08-30 14:11:56 +0200

    refs #40553: Interfaces for all actions

--- src/data/workflowActions/IApiAction.ts
+++ src/data/workflowActions/IApiAction.ts
@@ -1,3 +1,5 @@
+import { TFolderName } from "../../constants/FolderNames";
+
 export interface IApiEvent {
     TypeName: TActionEventType;
     Actions: TApiAction[]
@@ -20,9 +22,93 @@ interface IApiActionRelevant {
 
 export type TCompletionLevel = 'DoNothing' | 'CanIgnore' | 'CannotIgnore';
 
+export interface IApiAreEqualAction extends IApiAction {
+    __type: 'AreEqualAction:#EA';
+    ComparisonParameters: {
+        ExpectedValue: string | null;
+        ActualValue: string | null;
+        NotEqualMessage: string | null;
+    };
+}
+
+export interface IApiCreateRelationAction extends IApiAction {
+    __type: 'CreateRelationAction:#EA';
+    NewRelationParameters: {
+        RelatedItemFileAs: string | null;
+        RelatedFolderName: TFolderName | null;
+        CreateItemCopy: boolean | null;
+    };
+}
+
+export interface IApiCreateTaskAction extends IApiAction {
+    __type: 'CreateTaskAction:#EA';
+    TaskData: {
+        TypeEn: string | null;
+        Subject: string | null;
+        Delegator: string | null;
+        Solver: string | null;
+        DueDate: string | null;
+        ReminderDate: string | null;
+        ImportanceEn: string | null;
+        SuperiorItem: string | null;
+        Company: string | null;
+        Contact: string | null;
+        Relations: string[] | null;
+        OpenWindowWhenPossible: boolean | null;
+    };
+}
+
+export interface IApiDatabaseFieldIsNotEmptyAction extends IApiAction {
+    __type: 'DatabaseFieldIsNotEmptyAction:#EA';
+    FieldName: string | null;
+}
+
+export interface IApiCheckPresenceOfRelationAction extends IApiAction {
+    __type: 'CheckPresenceOfRelationAction:#EA';
+    CheckParameters: {
+        RelationType: string | null;
+        RelatedFolderName: TFolderName | null;
+        RelationMissingMessage: string | null;
+        RelatedItemConditions: {
+            FieldName: string;
+            Value: string | null;
+        }[] | null;
+    };
+}
+
+export interface IApiLockFieldAction extends IApiAction {
+    __type: 'LockFieldAction:#EA';
+    FieldName: string | null;
+}
+
+export interface IApiLockFormAction extends IApiAction {
+    __type: 'LockFormAction:#EA';
+}
+
+export interface IApiSendEmailAction extends IApiAction {
+    __type: 'SendEmailAction:#EA';
+    EmailData: {
+        Body: string | null;
+        Subject: string | null;
+        RecipientGroups: string | null;
+        RecipientUsers: string | null;
+    };
+}
+
+export interface IApiSetFieldValueAction extends IApiAction {
+    __type: 'SetFieldValueAction:#EA';
+    FieldName: string | null;
+    NewValue: string | null;
+}
+
+export interface IApiSetItemOwnerAction extends IApiAction {
+    __type: 'SetItemOwnerAction:#EA';
+    Message: string | null;
+}
+
 export interface IApiWriteJournalEntryAction extends IApiAction {
     __type: 'WriteJournalEntryAction:#EA';
-    Data: {
+    JournalData: {
         TypeEn: string | null;
         FileAs: string | null;
         Note: string | null;
