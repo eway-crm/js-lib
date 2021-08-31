12af3aec9e3eb3ac7ef9a400bb7af469c46395c6
Štěpán Havránek <stepan.havranek@eway-crm.com>
2021-08-31 13:48:14 +0200

    refs #40553: Easy vs advanced model flag

--- src/data/IApiWorkflowModel.ts
+++ src/data/IApiWorkflowModel.ts
@@ -3,5 +3,6 @@ import { IApiItemBase } from "./IApiItemBase";
 export interface IApiWorkflowModel extends IApiItemBase {
     EnumTypeGuid: string;
     IsVisible: boolean;
-    ParentEn: string;
+    ParentEn: string | null;
+    IsUsingFlows: boolean;
 }
