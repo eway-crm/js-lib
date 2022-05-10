7f39d414bb685ac7cb888fbf48963015cac04e6e
Aidam1 <adam.znamenacek@gmail.com>
2022-05-10 14:44:38 +0200

    refs #41905: Update CustomizationStatsItemKeys

--- src/constants/CustomizationStatsItemKeys.ts
+++ src/constants/CustomizationStatsItemKeys.ts
@@ -1,13 +1,11 @@
 export default class CustomizationStatsItemKeys {
     static additionalFieldsCount = 'AdditionalFieldsCount';
-    static customAdvancedWorkflowsCount = 'CustomAdvancedWorkflowsCount';
-    static customBasicWorkflowsCount = 'CustomBasicWorkflowsCount';
-    static customMandatoryOrUniqueFieldsCount = 'CustomMandatoryOrUniqueFieldsCount';
+    static customEnabledAdvancedWorkflowsCount = 'CustomEnabledAdvancedWorkflowsCount';
+    static customEnabledBasicWorkflowsCount = 'CustomEnabledBasicWorkflowsCount';
+    static customMandatoryFieldsCount = 'CustomMandatoryFieldsCount';
     static customOptionalFieldsCount = 'CustomOptionalFieldsCount';
-    static customTypesCount = 'CustomTypesCount';
-    static invisibleCurrenciesCount = 'InvisibleCurrenciesCount';
-    static systemAdvancedWorkflowsCount = 'SystemAdvancedWorkflowsCount';
-    static systemMandatoryOrUniqueFieldsCount = 'SystemMandatoryOrUniqueFieldsCount';
-    static systemTypesCount = 'SystemTypesCount';
+    static customReadonlyFieldsCount = 'CustomReadonlyFieldsCount';
+    static customUniqueFieldsCount = 'CustomUniqueFieldsCount';
+    static customVisibleTypesCount = 'CustomVisibleTypesCount';
     static visibleCurrenciesCount = 'VisibleCurrenciesCount';
 }
