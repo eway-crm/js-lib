0ee37b95ff42ef7f40a6d1bc2b986a558964307b
Štěpán Havránek <stepan.havranek@eway-crm.com>
2022-10-21 15:03:37 +0200

    refs #42808: New relation types and type

--- src/constants/RelationTypes.ts
+++ src/constants/RelationTypes.ts
@@ -1,4 +1,17 @@
+export type TRelationType = 'GENERAL'
+    | 'GROUP'
+    | 'CONTACTPERSON'
+    | 'CONTACT'
+    | 'CUSTOMER'
+    | 'COMPANY'
+    | 'SUPERVISOR';
+
 export default class RelationTypes {
-    static group = 'GROUP';
-    static supervisor = 'SUPERVISOR';
+    static readonly general: TRelationType = 'GENERAL';
+    static readonly group: TRelationType = 'GROUP';
+    static readonly contactPerson: TRelationType = 'CONTACTPERSON';
+    static readonly contact: TRelationType = 'CONTACT';
+    static readonly customer: TRelationType = 'CUSTOMER';
+    static readonly company: TRelationType = 'COMPANY';
+    static readonly supervisor: TRelationType = 'SUPERVISOR';
 }
