2d4e6a2eae64863216949f1ada7dcb742ee920d7
Štěpán Havránek <stepan.havranek@eway-crm.com>
2023-04-14 15:36:13 +0200

    refs #43883: Fix field name to match permission key

--- src/constants/FieldNames.ts
+++ src/constants/FieldNames.ts
@@ -27,7 +27,7 @@ export default class FieldNames {
     } as const;
 
     static readonly Companies = {
-        CID: 'CID',
+        ID: 'ID',
         CompanyName: 'CompanyName',
         Department: 'Department',
         AccountNumber: 'AccountNumber',
