e6286d8c87f7d2ace607b34b8a5f3ac037d4b2f9
Aidam1 <adam.znamenacek@gmail.com>
2023-09-19 08:50:19 +0200

    refs #44955: Add IsCloudHosted to IApiLicense

--- src/data/IApiLicense.ts
+++ src/data/IApiLicense.ts
@@ -64,6 +64,7 @@ export interface IApiLicense {
     Invoices: IApiLicenseKeyInvoices | null;
     GeneratedDate: string;
     DbSize: IApiLicenseDbSizeModel;
+    IsCloudHosted: boolean;
 }
 
 export interface IApiLicenseDbSizeModel {
