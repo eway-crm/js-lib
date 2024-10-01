e079176a51e631e20d2f3bb44400e12706462ee5
Lukáš Kolář <lukas.kolar@eway-crm.com>
2024-10-01 14:56:08 +0200

    refs #47689: Updated number string with actuall response string

--- src/constants/ImportResult.ts
+++ src/constants/ImportResult.ts
@@ -1,9 +1,9 @@
 
 export enum ImportResult {
-    Success = "0",
-    SuccessNoAction = "1",
-    Failure = "2",
-    FailurePermissionForbiden = "3",
-    FailureDuplicityFound = "4",
-    FailureItemAlreadyRemoved = "5",
+    Success = "Success",
+    SuccessNoAction = "Success_NoAction",
+    Failure = "Failed",
+    FailurePermissionForbiden = "Failed_PermissionForbiden",
+    FailureDuplicityFound = "Failed_DuplicityFound",
+    FailureItemAlreadyRemoved = "Failed_ItemAlreadyRemoved",
 }
