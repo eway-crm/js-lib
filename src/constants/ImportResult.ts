011c51ad5f4ad7ca8d567237dd669267cffa9b8b
Lukáš Kolář <lukas.kolar@eway-crm.com>
2024-10-01 12:13:51 +0200

    refs #47689: Added new interface for import data response and added new enum for importresult which serve as error entry

---
+++ src/constants/ImportResult.ts
@@ -0,0 +1,9 @@
+
+export enum ImportResult {
+    Success = "0",
+    SuccessNoAction = "1",
+    Failure = "2",
+    FailurePermissionForbiden = "3",
+    FailureDuplicityFound = "4",
+    FailureItemAlreadyRemoved = "5",
+}
