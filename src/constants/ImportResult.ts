3ad244d3abfaf709488f043004863a15a87fc8d6
Lukáš Kolář <lukas.kolar@eway-crm.com>
2024-11-07 12:58:45 +0100

    refs #47689: Updated import result keys

--- src/constants/ImportResult.ts
+++ src/constants/ImportResult.ts
@@ -1,9 +1,12 @@
 
 export enum ImportResult {
     Success = "Success",
-    SuccessNoAction = "Success_NoAction",
     Failure = "Failed",
-    FailurePermissionForbiden = "Failed_PermissionForbiden",
     FailureDuplicityFound = "Failed_DuplicityFound",
     FailureItemAlreadyRemoved = "Failed_ItemAlreadyRemoved",
+    FailureColumnsLockedWFAction = "Failed_ColumnsLocked",
+    FailureItemLockedWFAction = "Failed_ItemLocked",
+    FailureLicenceLimitReached = "Failed_LicenseLimitReached",
+    FailureInssuficientModulePermission = "Failed_InsufficientModulePermission",
+    FailureInssuficientColumnsPermission = "Failed_InsufficientColumnsPermission"
 }
