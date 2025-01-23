46a256298c4d7295dc8d974772d9fb533e5a8820
jspilka <jan.spilka@eway-crm.com>
2025-01-23 08:54:25 +0100

    refs #48115: Added new version to enumeration

--- src/helpers/VersionHelper.ts
+++ src/helpers/VersionHelper.ts
@@ -10,39 +10,44 @@ export enum Version {
     Version83 = '8.3',
     Version90 = '9.0',
     Version91 = '9.1',
+    Version92 = '9.2'
 }
 
 export default class VersionHelper extends VersionHelperBase {
     static readonly is75OrLater = (connection: ApiConnection) => {
-        return VersionHelperBase.supportsFeaturesOf(connection, "7.5");
+        return VersionHelperBase.supportsFeaturesOf(connection, Version.Version75);
     };
 
     static readonly is76OrLater = (connection: ApiConnection) => {
-        return VersionHelper.supportsFeaturesOf(connection, "7.6");
+        return VersionHelper.supportsFeaturesOf(connection, Version.Version76);
     };
 
     static readonly is77OrLater = (connection: ApiConnection) => {
-        return VersionHelper.supportsFeaturesOf(connection, "7.7");
+        return VersionHelper.supportsFeaturesOf(connection, Version.Version77);
     };
 
     static readonly is81OrLater = (connection: ApiConnection) => {
-        return VersionHelper.supportsFeaturesOf(connection, "8.1");
+        return VersionHelper.supportsFeaturesOf(connection, Version.Version81);
     };
 
     static readonly is82OrLater = (connection: ApiConnection) => {
-        return VersionHelper.supportsFeaturesOf(connection, "8.2");
+        return VersionHelper.supportsFeaturesOf(connection, Version.Version82);
     };
 
     static readonly is83OrLater = (connection: ApiConnection) => {
-        return VersionHelper.supportsFeaturesOf(connection, "8.3");
+        return VersionHelper.supportsFeaturesOf(connection, Version.Version83);
     };
 
     static readonly is90OrLater = (connection: ApiConnection) => {
-        return VersionHelper.supportsFeaturesOf(connection, "9.0");
+        return VersionHelper.supportsFeaturesOf(connection, Version.Version90);
     };
 
     static readonly is91OrLater = (connection: ApiConnection) => {
-        return VersionHelper.supportsFeaturesOf(connection, "9.1");
+        return VersionHelper.supportsFeaturesOf(connection, Version.Version91);
+    };
+
+    static readonly is92OrLater = (connection: ApiConnection) => {
+        return VersionHelper.supportsFeaturesOf(connection, Version.Version92);
     };
 
     static readonly isFeatureSupported = (connection: ApiConnection, featureOfVersion: Version) => {
